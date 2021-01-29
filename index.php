229f58f365a19f6da3a6ab85b7b8612c52718e01
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-01-29 12:26:12 +0100

    Exo-158-PHP

--- index.php
+++ index.php
@@ -3,9 +3,29 @@
 - Créer un script php qui permet de générer un nombre aléatoire compris entre x et y
 - Coté front, créer un formulaire acceptant deux valeurs : x et y
 - Envoyer le formulaire au script php et retourner le nombre aleatoire généré
-
-
 - Pour générer un nombre aléatoire en PHP , vous pouvez utiliser la méthode mt_rand() ou rand()
-
 http://php.net/manual/fr/function.mt-rand.php
 */
+
+
+function alea($min,$max): int{
+    return rand($min,$max);
+}
+
+if(isset($_POST["submit"])){
+    if(isset($_POST["min"]) && isset($_POST["max"])){
+        $min = intval(strip_tags($_POST["min"]));
+        $max = intval(strip_tags($_POST["max"]));
+        if($min > getrandmax()){
+            $min = getrandmax();
+        }
+        if($max > getrandmax())
+            $max = getrandmax();
+        if($min > $max){
+            echo alea($max,$min);
+        }
+        else{
+            echo alea($min,$max);
+        }
+    }
+}
